255db072dd75b80c3bda07c2cfcfa2e6b7516d5d
MarinaBoudin <marina.boudin@etu.u-bordeaux.fr>
2018-12-12 22:38:53 +0100

    new

--- main.js
+++ main.js
@@ -15,46 +15,88 @@ var movetop = function(plateau,base){
     plateau[pos[0]-1][pos[1]] = 2;
     plateau[pos[0]][pos[1]]=base[pos[0]][pos[1]];
   }
+  affichage(plateau);
 }
-var setupListener = function(plateau,base){
+var movebottom = function(plateau,base){
+  var pos = player(plateau);
+  var new_position = plateau[pos[0]+1][pos[1]];
+  if (new_position!=0){
+    plateau[pos[0]+1][pos[1]] = 2;
+    plateau[pos[0]][pos[1]]=base[pos[0]][pos[1]];
+  }
+  affichage(plateau);
+}
+var moveleft = function(plateau,base){
+  var pos = player(plateau);
+  var new_position = plateau[pos[0]][pos[1]-1];
+  if (new_position!=0){
+    plateau[pos[0]][pos[1]-1] = 2;
+    plateau[pos[0]][pos[1]]=base[pos[0]][pos[1]];
+  }
+  affichage(plateau);
+}
+var moveright = function(plateau,base){
+  var pos = player(plateau);
+  var new_position = plateau[pos[0]][pos[1]+1];
+  if (new_position!=0){
+    plateau[pos[0]][pos[1]+1] = 2;
+    plateau[pos[0]][pos[1]]=base[pos[0]][pos[1]];
+  }
+  affichage(plateau);
+}
+var affichage = function(plateau){
   var tree = document.getElementById("gameboard");
   if (tree.style.display!='none'){
+    var div = document.createElement("div");
+    var old_div = document.getElementById("plateau");
+    old_div.id="old";
+    old_div.style.display="none";
+    div.id="plateau";
+    div.style.width="1550px";
+    div.style.height="600px";
+    var gameboard = document.getElementById("gameboard");
+    gameboard.appendChild(div);
     for (var lign of plateau){
       for (var column of lign){
-        var newDiv = document.createElement("canvas");
+        var canvas_grid = document.createElement("canvas");
         if (column===0){
-          newDiv.style.backgroundColor="white";
+          canvas_grid.style.backgroundColor="white";
         }
         else if (column===1) {
-          newDiv.style.backgroundColor="black";
+          canvas_grid.style.backgroundColor="black";
         }
         else if (column===2) {
-          newDiv.style.backgroundColor="#0099ff";
+          canvas_grid.style.backgroundColor="#0099ff";
         }
         else if (column===3) {
-          newDiv.style.backgroundColor="#7300e6";
+          canvas_grid.style.backgroundColor="#7300e6";
         }
         else if (column==4){
-          newDiv.style.backgroundColor="#cc0000";
+          canvas_grid.style.backgroundColor="#cc0000";
         }
         else if (column==5){
-          newDiv.style.backgroundColor="#4c4c34";
+          canvas_grid.style.backgroundColor="#4c4c34";
         }
         else if (column==6){
-          newDiv.style.backgroundColor="#3d3d29";
+          canvas_grid.style.backgroundColor="#3d3d29";
         }
-        newDiv.style.width="50px";
-        newDiv.style.height="50px";
-        var div = document.getElementById("plateau");
-        div.appendChild(newDiv);
+        canvas_grid.style.width="50px";
+        canvas_grid.style.height="50px";
+        div.appendChild(canvas_grid);
       }
     }
   }
+}
+var setupListener = function(plateau,base){
+  affichage(plateau);
   var top = document.getElementById("Top");
-  top.addEventListener("click",movetop(plateau,base));
-  var botton = document.getElementById("Botton");
+  top.addEventListener("click",function(){movetop(plateau,base)});
+  var bottom = document.getElementById("Bottom");
+  bottom.addEventListener("click",function(){movebottom(plateau,base)});
   var right = document.getElementById("Right");
+  right.addEventListener("click",function(){moveright(plateau,base)});
   var left = document.getElementById("Left");
+  left.addEventListener("click",function(){moveleft(plateau,base)});
   var jack = document.getElementById("blackcat");
   jack.style.display="none";
 }
@@ -82,4 +124,4 @@ var base = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
               [0,0,0,0,0,0,0,0,0,0,0,1,3,1,1,1,4,1,1,1,1,1,0,0,0,0,0,0,0,0,0],
               [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
               [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]];
-window.addEventListener("load",setupListener(plateau,base));
+window.addEventListener("load",function(){setupListener(plateau,base)});
